f2d6635cc14ecf1952b6d58e4bb38d6362594888
AlexNovikovRussian <alexprogramer@mail.ru>
2019-12-28 18:46:22 +0300

    3

--- web/index.php
+++ web/index.php
@@ -5,21 +5,22 @@ require('../vendor/autoload.php');
 $app = new Silex\Application();
 $app['debug'] = true;
 
-// Register the monolog logging service
-// $app->register(new Silex\Provider\MonologServiceProvider(), array(
-//   'monolog.logfile' => 'php://stderr',
-// ));
+$app->post('/bot', function() use($app) {
+  $data = json_decode(file_get_contents('php://input'));
 
-// Register view rendering
-// $app->register(new Silex\Provider\TwigServiceProvider(), array(
-//     'twig.path' => __DIR__.'/views',
-// ));
+  if($data == ""){
+  	return "nioh";
+  }
 
-// Our web handlers
+  if($data->secret != getenv("VK_SECRET_TOKEN") && $data->type != "confirmation"){
+  	return "nioh";
+  }
 
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
+switch($data->type){
+	case "confirmation":
+		return getenv("VK_SERVER_AUTH_TOKEN");
+}
+
 });
 
 $app->run();

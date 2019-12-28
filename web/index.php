b2cef4568b8d5cf2dac2e47141d7a7c5bc6b73c0
AlexNovikovRussian <alexprogramer@mail.ru>
2019-12-28 17:49:34 +0300

    3

--- web/index.php
+++ web/index.php
@@ -6,14 +6,14 @@ $app = new Silex\Application();
 $app['debug'] = true;
 
 // Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
+// $app->register(new Silex\Provider\MonologServiceProvider(), array(
+//   'monolog.logfile' => 'php://stderr',
+// ));
 
 // Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
+// $app->register(new Silex\Provider\TwigServiceProvider(), array(
+//     'twig.path' => __DIR__.'/views',
+// ));
 
 // Our web handlers
 

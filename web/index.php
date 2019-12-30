26bd986107f1f482142b9e32af04e043378cb62c
AlexNovikovRussian <alexprogramer@mail.ru>
2019-12-30 14:14:23 +0300

    04

--- web/index.php
+++ web/index.php
@@ -5,6 +5,10 @@ require('../vendor/autoload.php');
 $app = new Silex\Application();
 $app['debug'] = true;
 
+$app->get('/', function() use($app){
+	return "Hello, world!"
+});
+
 $app->post('/bot', function() use($app) {
   $data = json_decode(file_get_contents('php://input'));
 
@@ -19,6 +23,13 @@ $app->post('/bot', function() use($app) {
 switch($data->type){
 	case "confirmation":
 		return getenv("VK_SERVER_AUTH_TOKEN");
+
+
+	case "message_new":
+		$params = array(
+			"access_token" => getenv(VK_GROUP_TOKEN),
+			"v" => "5.69",
+			"user_id" => ""
 }
 
 });
